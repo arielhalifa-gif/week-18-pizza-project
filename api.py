e378a651e3647bacc2f2b3c2bb737a1c96ebb9af
arielhalifa <arielhalifa@gmail.com>
2026-02-17 09:34:37 +0200

    api file updated

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import json
 import uvicorn
 from pydantic import BaseModel, Field
 from typing import Optional
 import uuid
 from mongo.connection import get_collection
+from kafka.producer import send_to_kafka
 
 
 class Order(BaseModel):
@@ -15,23 +17,39 @@ class Order(BaseModel):
     special_instructions: Optional[str] = None
 
 
-app = FastAPI()
-
-@app.post("/uploadfile")
-def uploading_file(order: Order):
-    data_mongo = {"order_id": order.order_id,
+def get_mongo_data(order: Order):
+    data_mongo = {"order_id": str(order.order_id),
                   "pizza_type": order.pizza_type,
                   "size": order.size,
                   "quantity": order.quantity,
                   "is_delivery": order.is_delivery,
                   "special_instructions": order.special_instructions,
                   "status": "PREPARING"}
+    return data_mongo
+
+app = FastAPI()
+
+@app.post("/uploadfile")
+def uploading_file(file: UploadFile = File(...)):
     collection = get_collection()
-    inserted = collection.insert_one(data_mongo)
-    data_mongo['_id'] = str(data_mongo['_id'])
-    return {"message": "data inserted into mongo",
-            "data": data_mongo}
+    order_load = json.load(file.file)
+    order_load = [order_load]
+    count = 0
+    for order in order_load:
+        data_mongo = get_mongo_data(order)
+        inserted = collection.insert_one(data_mongo)
+        data_mongo['_id'] = str(data_mongo['_id'])
+        count += 1
+        send_to_kafka(data_mongo)
+    return {"message": "data inserted successfully",
+            "total": f"{count} orders inserted"}
+
+
+@app.get("/order/{order_id}")
+def get_by_id(order_id):
+    pass
+
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port="8000")
+    uvicorn.run(app, host="0.0.0.0", port="8000")
